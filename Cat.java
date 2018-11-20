9c36753c8a79def8b5d22cb3a85f10cb66760aff
Ian <19daviesi@flintridgeprep.org>
2018-11-19 22:30:14 -0800

    created testcat

--- Cat.java
+++ Cat.java
@@ -1,3 +1,28 @@
 
 public class Cat {
+    private String name;
+    private double weight;
+    
+    void setName (String aName){
+        name = aName; 
+    }
+    void setWeight (double aName){
+        weight = aName;
+    }
+    
+    String getName (){
+        return name;
+    }
+    double getWeight(){
+        return weight;
+    }
+    
+    public Cat(){//constructor
+        
+    }
+    public Cat(String aName){
+        name = aName;
+    }
+    
+    void meow()
 }
