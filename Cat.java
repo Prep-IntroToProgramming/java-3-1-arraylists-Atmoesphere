348955fe651f35bd87e11fe2e15ab130a054ba7f
Ian <19daviesi@flintridgeprep.org>
2018-11-20 11:00:19 -0800

    completed

--- Cat.java
+++ Cat.java
@@ -2,27 +2,32 @@
 public class Cat {
     private String name;
     private double weight;
-    
+
     void setName (String aName){
         name = aName; 
     }
+
     void setWeight (double aName){
         weight = aName;
     }
-    
+
     String getName (){
         return name;
     }
+
     double getWeight(){
         return weight;
     }
-    
+
     public Cat(){//constructor
-        
+
     }
+
     public Cat(String aName){
         name = aName;
     }
-    
-    void meow()
+
+    void meow(){
+        System.out.println("meow");
+    }
 }
--- TestCat.java
+++ TestCat.java
@@ -1,33 +1,30 @@
-
-/**
- * Write a description of class TestCat here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class TestCat
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class TestCat
-     */
-    public TestCat()
-    {
-        // initialise instance variables
-        x = 0;
+import java.util.*;
+public class TestCat {
+    public static void main (String[] args){
+    ArrayList<Cat> CatList = new ArrayList<Cat>(); // <> for array lists
+    Cat cat1 = new Cat("cat1"); //created new cat
+    Cat cat2 = new Cat("cat2"); //created new cat
+    Cat cat3 = new Cat("cat3"); //created new cat
+    Cat cat4 = new Cat("cat4"); //created new cat
+    
+    CatList.add(0,cat1);//names for cat constructors
+    CatList.add(1,cat2);
+    CatList.add(2,cat3);
+    CatList.add(3,cat4);
+    
+    int x=0;
+    for (Cat i: CatList){
+        double w = (Math.random()*10)+5;
+        CatList.get(x).setWeight(w);
+        x = x + 1;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    x = 0; 
+    for (Cat i: CatList){
+        System.out.println(CatList.get(x).getName() + " " + CatList.get(x).getWeight());
+        System.out.println(CatList.get(x).getName() + " says ");
+        CatList.get(x).meow();
+        x = x + 1;
     }
+}
 }
